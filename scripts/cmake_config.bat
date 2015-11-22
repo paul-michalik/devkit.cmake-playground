039aaddf15ab0224245815b81f86e086636945bb
Paul Michalik <paul.michalik@outlook.com>
2015-11-22 14:15:35 +0100

    Prepared for composing cmake/cmake-gui command line.

--- scripts/cmake_config.bat
+++ scripts/cmake_config.bat
@@ -20,8 +20,11 @@ echo calling "%Package_RootDir%\scripts\cmake_package_generator.bat"
 call "%Package_RootDir%\scripts\cmake_package_generator.bat"
  
 set "Package_SrcDir=%Package_RootDir%\src\%Package_Name%"
+
 set "Package_BuildDir=%Package_RootDir%\build\%Package_Name%.%Package_Generator%.%Package_Platform%.%Package_Toolset%.%Package_Configuration%"
 
-set Package
+set "Package_CMakeCmdLine="
+
+set Package_
 
 endlocal
